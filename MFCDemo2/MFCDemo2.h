2899b30aba57b0f76337530a04dd8cb3725d233c
陈明智 <throughthejugnle@163.com>
2021-02-25 09:17:28 +0800

    Add project files.

---
+++ MFCDemo2/MFCDemo2.h
@@ -0,0 +1,32 @@
+
+// MFCDemo2.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCDemo2App:
+// See MFCDemo2.cpp for the implementation of this class
+//
+
+class CMFCDemo2App : public CWinApp
+{
+public:
+	CMFCDemo2App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCDemo2App theApp;
